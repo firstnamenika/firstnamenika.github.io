683c0d9697a7a1eb7d3ce63f04dc33a6329ab4d4
Nika Kolesnikova <bluedotveronika@gmail.com>
2024-01-12 10:09:28 -0500

    styled

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 window.onscroll = function() {
   // Add or remove the 'scrolled' class based on scroll position
-  document.body.classList.toggle('scrolled', window.scrollY > 20);
+  document.body.classList.toggle('scrolled', window.scrollY > 200);
 };
 
 // Smooth scroll to top when the anchor is clicked
-document.getElementById("top").addEventListener("click", function() {
+document.getElementById("top-anchor").addEventListener("click", function() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 });
