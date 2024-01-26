6d75f89a175a50ad1cc43af4f2f670a068aecf5e
Nika Kolesnikova <bluedotveronika@gmail.com>
2024-01-26 14:32:23 -0500

    updated projects section & added certifications section

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Add slides
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  return slideIndex
+});
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  // Thumbnail image controls
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("project-card");
+    console.log("Number of slides:", slides.length);
+  
+    if (slides.length === 0) {
+      console.log("No cards in this array");
+      return; // Exit the function to avoid further errors
+    }
+  
+    slideIndex = (n > slides.length) ? 1 : (n < 1) ? slides.length : n;
+  
+    console.log("Setting display to block for slide:", slideIndex);
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
+  
+
+
+
+
 window.onscroll = function() {
   // Add or remove the 'scrolled' class based on scroll position
   document.body.classList.toggle('scrolled', window.scrollY > 200);
@@ -8,3 +47,5 @@ document.getElementById("top-anchor").addEventListener("click", function() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 });
+
+
